5c4ce18af2380760ae66060f1226aab8e2ecf5d1
Milos Katic <46404864+BogoEngineer@users.noreply.github.com>
2019-12-14 16:38:02 +0100

    Add files via upload
    Game adjusted for more optimal learning via NEAT algorithm.

--- Snake.py
+++ Snake.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 random.seed()
 
 
@@ -9,15 +9,22 @@ class Ground:
         self.y = 0
         self.width = width
         self.height = height
-        self. fruit = [random.randint(self.x, self.x+self.width-1), random.randint(self.y, self.y+self.height-1)]
+        #self. fruit = [random.randint(self.x, self.x+self.width-1), random.randint(self.y, self.y+self.height-1)] #x,y coordinates
+
+        self.fruit = [random.randint(self.x, self.x + self.width - 2), random.randint(self.y, self.y + self.height - 2)]
 
     def generateFruit(self):
-        self.fruit =  [random.randint(self.x, self.x+self.width-1), random.randint(self.y, self.y+self.height-1)]
+        self.fruit =  [random.randint(self.x, self.x+self.width-2), random.randint(self.y, self.y+self.height-2)]
 
 
 
 class Snake:
+    id = 0;
     def __init__(self, xHead, yHead):
+        Snake.id+=1
+        self.hungry_for = 0 #for how many time units snake hasnt eat
+        self.id = Snake.id
+        #self.speedX = 2.5
         self.speedX = 1
         self.speedY = 0
         self.body = [[xHead, yHead]]
@@ -50,9 +57,16 @@ class Snake:
         else: return True
 
     def wallCrashed(self, ground):
-        if(self.body[0][0] < ground.x or self.body[0][0]>(ground.x + ground.width) or self.body[0][1] < ground.y or self.body[0][1] > (ground.y + ground.height)):
-            return True
-        else: return False
+        if self.body[0][0] < ground.x:
+            self.body[0][0] += ground.width
+        if self.body[0][0]>(ground.x + ground.width):
+            self.body[0][0] -= ground.width
+        if self.body[0][1] < ground.y:
+            self.body[0][1] += ground.height
+        if self.body[0][1] > (ground.y + ground.height):
+            self.body[0][1] -= ground.height
+        return False
+
 
     def raveled(self):
         for i, part in enumerate(self.body):
@@ -71,7 +85,7 @@ class Snake:
     """
 
     def eat(self, ground):
-        if (self.body[0][0] == ground.fruit[0] and self.body[0][1] == ground.fruit[1]):
+        if (self.body[0][0] == ground.fruit[0] or self.body[0][0] == ground.fruit[0] + 1) and (self.body[0][1] == ground.fruit[1] or self.body[0][1] == ground.fruit[1]+1):
             if(len(self.body) == 1):
                 self.body.append([self.body[0][0] - self.speedX, self.body[0][1] - self.speedY])
             elif(self.body[-2][0] == self.body[-1][0]):
@@ -79,6 +93,16 @@ class Snake:
             elif(self.body[-2][1] == self.body[-2][1]):
                 self.body.append([self.body[-1][0], self.body[-2][1] - self.body[-1][1] + self.body[-1][1]])
             self.score += 1
+            self.hungry_for = 0
             ground.generateFruit()
             return True
+        self.hungry_for += 1
         return False
+
+    def distance(self, ground):
+        x1 = self.body[0][0]
+        y1 = self.body[0][1]
+        x2 = ground.fruit[0]
+        y2 = ground.fruit[1]
+
+        return math.sqrt((x1-x2)**2 + (y1-y2)**2)
